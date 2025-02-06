0a1acefa60e4acb29dee0efeb65fe43fd05ad4a1
Marco <46133335+Marco4413@users.noreply.github.com>
2025-02-06 08:52:42 +0100

    Fixed std::from_chars errors because of signed type
    Char may overflow and become negative.
    std::from_chars probably expects '-' so it errors.

--- lsp/fileuri.cpp
+++ lsp/fileuri.cpp
@@ -66,7 +66,7 @@ std::string FileURI::decode(std::string_view encoded)
 			const char* start = &encoded[i + 1];
 			const char* end = &encoded[i + 3];
 
-			char c;
+			unsigned char c;
 			const auto [ptr, ec] = std::from_chars(start, end, c, 16);
 
 			if(ec != std::errc{} || ptr != end)
